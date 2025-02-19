cfac8ca0d9323c76b3a6568a1813da065cc545e7
Alfredchanja <alfredgachanja8@gmail.com>
2025-02-19 23:57:46 +0300

    Create a grid

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+
+let i = 1;
+while (i < 17) {
+    const item1 = document.createElement("div");
+    container.appendChild(item1);
+    i++;
+}
